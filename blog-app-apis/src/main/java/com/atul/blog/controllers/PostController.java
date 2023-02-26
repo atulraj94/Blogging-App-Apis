41444b80dfd353f78f7367066457891a6d703c3a
atulraj94 <atulraj9471@gmail.com>
2023-02-26 15:32:54 +0530

    "code for file upload in folder and serve"

--- blog-app-apis/src/main/java/com/atul/blog/controllers/PostController.java
+++ blog-app-apis/src/main/java/com/atul/blog/controllers/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.atul.blog.config.AppConstant;
 import com.atul.blog.payloads.ApiResponse;
 import com.atul.blog.payloads.PostDto;
 import com.atul.blog.payloads.PostResponse;
@@ -57,9 +58,9 @@ public class PostController {
 	//get all post
 		@GetMapping("/posts")
 		public ResponseEntity<PostResponse> getAllPost(
-				           @RequestParam(value="pageNumber", defaultValue="0",required=false) Integer pageNumber,
-				           @RequestParam(value="pageSize",defaultValue="5",required= false) Integer pageSize,
-				           @RequestParam(value="sortBy",defaultValue="postId",required=false) String sortBy){
+				           @RequestParam(value="pageNumber", defaultValue=AppConstant.PAGE_NUMBER ,required=false) Integer pageNumber,
+				           @RequestParam(value="pageSize",defaultValue=AppConstant.PAGE_SIZE,required= false) Integer pageSize,
+				           @RequestParam(value="sortBy",defaultValue=AppConstant.SORT_BY,required=false) String sortBy){
 			PostResponse response = this.postservice.getAllPost(pageNumber, pageSize,sortBy);
 			return new ResponseEntity<PostResponse>(response, HttpStatus.OK);
 		}
